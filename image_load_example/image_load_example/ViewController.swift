881d19ee356c5edc474a19c71411773dee8176e3
g-ko <kyounghyunko@daka-no-Mac-mini.local>
2019-03-14 13:05:56 +0900

    image load sample

---
+++ image_load_example/image_load_example/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  image_load_example
+//
+//  Created by 高 景賢 on 2019/01/25.
+//  Copyright © 2019年 cries. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /// キャッシュ画像保存用ローカルパス
+    var cacheDirectory: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.text = "hello world!"
+        
+        if let image = getSavedImage(named: "fileName") {
+            // do something with image
+            textView.text = "イメージ読み込み"
+            print("イメージ読み込み")
+            imageView.image = image
+            
+        } else {
+            let success = saveImage(image: UIImage(named: "myimage.png")!)
+            
+            if success {
+                textView.text = "イメージ保存成功"
+                print("イメージ保存成功")
+                if let image = getSavedImage(named: "fileName") {
+                    // do something with image
+                    textView.text = "イメージ読み込み(保存後)"
+                    print("イメージ読み込み(保存後)")
+                    imageView.image = image
+                }
+            } else {
+                textView.text = "イメージ保存失敗"
+                print("イメージ保存失敗")
+            }
+        }
+        
+        
+        
+    }
+
+    func getSavedImage(named: String) -> UIImage? {
+        if let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) {
+            return UIImage(contentsOfFile: URL(fileURLWithPath: dir.absoluteString).appendingPathComponent(named).path)
+        }
+        return nil
+    }
+    
+    func saveImage(image: UIImage) -> Bool {
+        guard let data = image.jpegData(compressionQuality: 1.0) ?? image.pngData() else {
+            return false
+        }
+        guard let directory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) as NSURL else {
+            return false
+        }
+        do {
+            try data.write(to: directory.appendingPathComponent("fileName.png")!)
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+
+
+}
+
---
+++ image_load_example/image_load_exampleTests/image_load_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  image_load_exampleTests.swift
+//  image_load_exampleTests
+//
+//  Created by 高 景賢 on 2019/01/25.
+//  Copyright © 2019年 cries. All rights reserved.
+//
+
+import XCTest
+@testable import image_load_example
+
+class image_load_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
